7d6cb4ea41848ca8c520453c6bbfe6591750c9ec
Mert Yeşilbaş <mert.yesilbas.888@hotmail.com>
2022-08-08 21:08:19 +0300

    Created HaberlerActivity.kt and XML file and Renamed MainAcitivity as KullaniciActivity

--- app/src/main/java/com/example/btkfotografpaylasmafirebase/HaberlerActivity.kt
+++ app/src/main/java/com/example/btkfotografpaylasmafirebase/HaberlerActivity.kt
@@ -2,12 +2,10 @@ package com.example.btkfotografpaylasmafirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class HaberlerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        setContentView(R.layout.activity_haberler)
     }
 }
---
+++ app/src/main/java/com/example/btkfotografpaylasmafirebase/KullaniciActivity.kt
@@ -0,0 +1,22 @@
+package com.example.btkfotografpaylasmafirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.firebase.ktx.Firebase
+
+class KullaniciActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun girisYap(view: View){
+
+    }
+
+    fun kayitOl(view: View){
+
+    }
+}
