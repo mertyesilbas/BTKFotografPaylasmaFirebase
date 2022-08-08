c12a4a9c6577f2629a77783a397d7bdf4633e494
Mert Yeşilbaş <mert.yesilbas.888@hotmail.com>
2022-08-08 20:54:21 +0300

    Added Firebase Integration

--- app/src/main/java/com/example/btkfotografpaylasmafirebase/MainActivity.kt
+++ app/src/main/java/com/example/btkfotografpaylasmafirebase/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.btkfotografpaylasmafirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
